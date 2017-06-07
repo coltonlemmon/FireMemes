340962a6f04cdc499914bb1a521f8b1f7e97092b
Jose Melendez <JoseM_Melendez@icloud.com>
2017-06-07 12:07:13 -0600

    Added a tableViewCell File and a TableViewController file and subclassed them and added them in a folder called ViewControllers

---
+++ FireMemes/FireMemes/ShowMemesTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ShowMemesTableViewController.swift
+//  FireMemes
+//
+//  Created by Jose Melendez on 6/7/17.
+//  Copyright © 2017 Colton. All rights reserved.
+//
+
+import UIKit
+
+class ShowMemesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+ 
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "memeFeed", for: indexPath)
+
+
+        return cell
+    }
+ 
+
+
+}
